18951b6ce4b242dde064b59242add2a6b4afb2d4
thomaspmtm <thomaspmtm@yahoo.com>
2019-08-09 21:18:01 -0500

    Committed 8-9-2019

---
+++ er_javacode/src/main/java/com/example/er_javacode/er_javacode/ErJavacodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.er_javacode.er_javacode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErJavacodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErJavacodeApplication.class, args);
+	}
+
+}
---
+++ er_javacode/src/main/java/com/example/er_javacode/er_javacode/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.er_javacode.er_javacode;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ErJavacodeApplication.class);
+	}
+
+}
